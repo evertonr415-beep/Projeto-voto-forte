94ed36fe8ed1e71e7ad6087ebd396ee0000b7631
evertonr415-beep <evertonr415@gmail.com>
2026-08-05 20:15:06 -0300

    Força atualização da página de pendências

--- app/pendencias-localizacao/page.tsx
+++ app/pendencias-localizacao/page.tsx
@@ -2,6 +2,9 @@ import { redirect } from "next/navigation";
 import { getAccount } from "../server-identity";
 import LocationIssuesClient from "./location-issues-client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function LocationIssuesPage() {
   const account = await getAccount();
   if (!account) redirect("/contatos");
