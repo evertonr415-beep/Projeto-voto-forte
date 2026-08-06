84d7ebd554a5360ba8bdbdd0c9ec140e54e91bc1
evertonr415-beep <evertonr415@gmail.com>
2026-08-06 10:34:30 -0300

    style: load contact quality shortcut label

--- app/contatos/page.tsx
+++ app/contatos/page.tsx
@@ -1,5 +1,6 @@
 import OptimizedAuthClient from "../optimized-auth-client";
 import "./coverage-clarity.css";
+import "./contact-quality-label.css";
 
 export default function ContactsPage() {
   return <OptimizedAuthClient />;
