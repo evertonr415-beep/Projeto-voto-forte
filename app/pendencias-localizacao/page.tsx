668afc242aebb22c593d6e0d0da6081088437911
evertonr415-beep <evertonr415@gmail.com>
2026-08-08 16:24:24 -0300

    Ativa retorno discreto na Central de Qualidade

--- app/pendencias-localizacao/page.tsx
+++ app/pendencias-localizacao/page.tsx
@@ -1,11 +1,13 @@
 import LocationIssuesAuthClient from "./location-issues-auth-client";
 import QualityMobileListCollapse from "./quality-mobile-list-collapse";
+import QualityBackNavigation from "./quality-back-navigation";
 
 export default function LocationIssuesPage() {
   return (
     <>
       <LocationIssuesAuthClient />
       <QualityMobileListCollapse />
+      <QualityBackNavigation />
     </>
   );
 }
