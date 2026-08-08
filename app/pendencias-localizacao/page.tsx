b380c8ccf3f2fd4af788c3f974607c33d76ac2bb
evertonr415-beep <evertonr415@gmail.com>
2026-08-08 16:13:41 -0300

    Ativa lista compacta mobile na Central de Qualidade

--- app/pendencias-localizacao/page.tsx
+++ app/pendencias-localizacao/page.tsx
@@ -1,5 +1,11 @@
 import LocationIssuesAuthClient from "./location-issues-auth-client";
+import QualityMobileListCollapse from "./quality-mobile-list-collapse";
 
 export default function LocationIssuesPage() {
-  return <LocationIssuesAuthClient />;
+  return (
+    <>
+      <LocationIssuesAuthClient />
+      <QualityMobileListCollapse />
+    </>
+  );
 }
