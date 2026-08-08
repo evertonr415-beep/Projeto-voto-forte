ddc593d0b5bbb10394e8b427ca7beb4dd38e15d1
evertonr415-beep <evertonr415@gmail.com>
2026-08-08 16:03:20 -0300

    Ativa lista compacta de contatos no mobile

--- app/contatos/page.tsx
+++ app/contatos/page.tsx
@@ -1,6 +1,7 @@
 import AuthClient from "../auth-client";
 import ContactBackNavigation from "../contact-back-navigation";
 import ContactDistrictRanking from "../contact-district-ranking";
+import ContactMobileListCollapse from "../contact-mobile-list-collapse";
 import "./coverage-clarity.css";
 import "./contact-quality-label.css";
 
@@ -10,6 +11,7 @@ export default function ContactsPage() {
       <AuthClient dashboardMode="neutral" />
       <ContactBackNavigation />
       <ContactDistrictRanking />
+      <ContactMobileListCollapse />
     </>
   );
 }
