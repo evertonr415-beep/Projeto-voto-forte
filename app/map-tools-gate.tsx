b38c0947054dd4d4c5dfd229f8a5d7e01f4dd7ab
evertonr415-beep <evertonr415@gmail.com>
2026-08-09 16:33:56 -0300

    Ativa controles compactos do mapa no celular

--- app/map-tools-gate.tsx
+++ app/map-tools-gate.tsx
@@ -2,12 +2,14 @@
 
 import MapContactLayer from "./map-contact-layer";
 import MapTerritoryEnhancer from "./map-territory-enhancer";
+import MobileMapControls from "./mobile-map-controls";
 
 export default function MapToolsGate() {
   return (
     <>
       <MapContactLayer />
       <MapTerritoryEnhancer />
+      <MobileMapControls />
     </>
   );
 }
