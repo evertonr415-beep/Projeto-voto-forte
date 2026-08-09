0d76580ce8d1c8bd2158f4f87e7b44e2c8992340
evertonr415-beep <evertonr415@gmail.com>
2026-08-09 12:04:36 -0300

    Monta camada distrital separada no mapa

--- app/map-tools-gate.tsx
+++ app/map-tools-gate.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import MapContactLayer from "./map-contact-layer";
+import MapDistrictMarkerLayer from "./map-district-marker-layer";
 
 export default function MapToolsGate() {
-  return <MapContactLayer />;
+  return (
+    <>
+      <MapContactLayer />
+      <MapDistrictMarkerLayer />
+    </>
+  );
 }
