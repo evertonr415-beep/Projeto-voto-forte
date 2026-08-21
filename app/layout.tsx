755243c352563602840024744d2fbd491d3a054a
evertonr415-beep <evertonr415@gmail.com>
2026-08-21 14:07:57 -0300

    feat(ui): ativa navegacao voltar padrao

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ import WhaticketBroadcastDrawer from "./whaticket-broadcast-drawer";
 import TseSidebarEnhancer from "./tse-sidebar-enhancer";
 import ComunicacaoSidebarEnhancer from "./comunicacao-sidebar-enhancer";
 import SystemNotificationsDrawer from "./system-notifications-drawer";
+import StandardBackNavigationEnhancer from "./standard-back-navigation-enhancer";
 import PwaInstaller from "./pwa-installer";
 import "./globals.css";
 import "./compact-overview-scope.css";
@@ -132,6 +133,7 @@ export default function RootLayout({
         <WhaticketBroadcastDrawer />
         <TseSidebarEnhancer />
         <ComunicacaoSidebarEnhancer />
+        <StandardBackNavigationEnhancer />
         <SystemNotificationsDrawer />
         {children}
       </body>
