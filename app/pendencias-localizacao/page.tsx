59b9b09e2d9a7aba3792c658e395512d733c7de4
evertonr415-beep <evertonr415@gmail.com>
2026-08-23 10:57:59 -0300

    fix(access): manter validacao de rota no cliente autenticado

--- app/pendencias-localizacao/page.tsx
+++ app/pendencias-localizacao/page.tsx
@@ -1,16 +1,8 @@
-import { redirect } from "next/navigation";
-import { getAccount } from "../server-identity";
 import LocationIssuesAuthClient from "./location-issues-auth-client";
 import QualityMobileListCollapse from "./quality-mobile-list-collapse";
 import QualityBackNavigation from "./quality-back-navigation";
 
-export default async function LocationIssuesPage() {
-  const account = await getAccount();
-
-  if (!account || account.accessRole !== "adm") {
-    redirect("/");
-  }
-
+export default function LocationIssuesPage() {
   return (
     <>
       <LocationIssuesAuthClient />
