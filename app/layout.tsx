ac1bd0b2db2f063b936fd66829472c7ce716c1fc
evertonr415-beep <evertonr415@gmail.com>
2026-08-26 12:44:12 -0300

    style: carrega microinteracao da bandeira

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,7 @@ import "./contacts-final-mobile-stability.css";
 import "./whatsapp-mobile-cleanup.css";
 import "./administration-cleanup.css";
 import "./administration-mobile-header-fit.css";
+import "./parana-logo-microinteraction.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
