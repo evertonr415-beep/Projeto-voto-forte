42426ecdff7b5105676791d13480d990478fbdd7
evertonr415-beep <evertonr415@gmail.com>
2026-09-01 10:58:06 -0300

    Administração: ativar lista unificada de gestores

--- app/dashboard-runtime-enhancers.tsx
+++ app/dashboard-runtime-enhancers.tsx
@@ -24,6 +24,7 @@ import RuntimeVersionGuard from "./runtime-version-guard";
 import AuthReconciliationEnhancer from "./auth-reconciliation-enhancer";
 import TeamPerformanceAdminEnhancer from "./team-performance-admin-enhancer";
 import AdministrationActivityEnhancer from "./administration-activity-enhancer";
+import GestorAdministrationIdentityNormalizer from "./gestor-administration-identity-normalizer";
 import WhaticketBroadcastDrawer from "./whaticket-broadcast-drawer";
 import BroadcastNeighborhoodOptionsEnhancer from "./broadcast-neighborhood-options-enhancer";
 import TseSidebarEnhancer from "./tse-sidebar-enhancer";
@@ -91,6 +92,7 @@ export default function DashboardRuntimeEnhancers() {
           <AuthReconciliationEnhancer />
           <TeamPerformanceAdminEnhancer />
           <AdministrationActivityEnhancer />
+          <GestorAdministrationIdentityNormalizer />
           <RuntimeVersionGuard />
           <WhaticketBroadcastDrawer />
           <BroadcastNeighborhoodOptionsEnhancer />
