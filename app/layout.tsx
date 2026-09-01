2a1b702ee946c7d82cc7ed17fef6829870f6fb97
evertonr415-beep <evertonr415@gmail.com>
2026-08-31 21:29:29 -0300

    Visão Geral: cadastro genérico no atalho
    Publica o Preview aprovado do modal Cadastro na Visão Geral: perfil selecionável entre Eleitor/Liderança, Eleitor como padrão, botão Cancelar com contraste e ajuste mobile isolado.

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ import "./map-mobile-compact-experience.css";
 import "./overview-mobile-visual-polish.css";
 import "./overview-mobile-header-final.css";
 import "./overview-mobile-density-fix.css";
+import "./overview-leadership-modal-mobile.css";
 import "./map-mobile-header-compact.css";
 import "./mobile-tab-header-standard.css";
 import "./primary-mobile-header-standard.css";
