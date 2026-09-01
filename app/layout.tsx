9a1812593c201e601fbc71378c368dc572f74c2c
evertonr415-beep <evertonr415@gmail.com>
2026-09-01 10:45:43 -0300

    Administração: carregar estilos do preview de auditoria

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,6 +82,7 @@ import "./desktop-preview-recovery-fixes.css";
 import "./desktop-four-point-fixes.css";
 import "./desktop-agenda-export-fixes.css";
 import "./administration-initial-loading-guard.css";
+import "./administration-activity-enhancer.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
