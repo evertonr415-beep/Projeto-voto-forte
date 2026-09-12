335e33a56457649505827d40239c11b54b446359
evertonr415-beep <evertonr415@gmail.com>
2026-09-12 18:05:36 -0300

    Remove obsolete administration observers

--- app/dashboard-runtime-enhancers.tsx
+++ app/dashboard-runtime-enhancers.tsx
@@ -22,9 +22,6 @@ import CompactOverviewScopeEnhancer from "./compact-overview-scope-enhancer";
 import NetworkScopeLabelNormalizer from "./network-scope-label-normalizer";
 import RuntimeVersionGuard from "./runtime-version-guard";
 import AuthReconciliationEnhancer from "./auth-reconciliation-enhancer";
-import TeamPerformanceAdminEnhancer from "./team-performance-admin-enhancer";
-import AdministrationActivityEnhancer from "./administration-activity-enhancer";
-import AdministrationUxRedesignEnhancer from "./administration-ux-redesign-enhancer";
 import GestorAdministrationIdentityNormalizer from "./gestor-administration-identity-normalizer";
 import AdministrationKpiRoleSync from "./administration-kpi-role-sync";
 import WhaticketBroadcastDrawer from "./whaticket-broadcast-drawer";
@@ -111,9 +108,6 @@ export default function DashboardRuntimeEnhancers() {
           <ContactExportEnhancer />
           <IntelligenceNavigation />
           <AuthReconciliationEnhancer />
-          <TeamPerformanceAdminEnhancer />
-          <AdministrationActivityEnhancer />
-          <AdministrationUxRedesignEnhancer />
           <GestorAdministrationIdentityNormalizer />
           <AdministrationKpiRoleSync />
           <RuntimeVersionGuard />
