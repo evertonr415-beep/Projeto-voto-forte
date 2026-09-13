100ea8780047afd7f150ba6f6fa36a20a4656bc4
evertonr415-beep <evertonr415@gmail.com>
2026-09-13 19:31:32 -0300

    admin v9: ativar refinamentos da aba Acessos

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,7 @@ import "./administration-directory-actions.css";
 import "./administration-v6.css";
 import "./administration-v7.css";
 import "./administration-v8.css";
+import "./administration-v9.css";
 import "./pwa-installed-safe-area.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
